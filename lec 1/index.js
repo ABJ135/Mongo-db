53ce57baabe9a904d9d25f8781e870ceb0cd31ff
ABJ135 <1515.abubakarjaved.m2@gmail.com>
2024-11-13 19:37:59 +0500

    server and mogoose connection

--- lec 1/index.js
+++ lec 1/index.js
@@ -1,17 +1,21 @@
 //initial initialization for server
-const express = require('express')
-const app = express()
-app.use(express.json())
-const port = 3000
+const express = require("express");
+const app = express();
+app.use(express.json());
+const port = 3000;
 
 //initializing mongo db
-const mongoose = require('mongoose')
-const {Schema} = require('mongoose')
+const mongoose = require("mongoose");
+const { Schema } = require("mongoose");
 
 //connection testing with mongo db
-mongoose.connect('mongodb://localhost:27017/library').then(()=>console.log('Data base created'))
+mongoose
+  .connect("mongodb://localhost:27017/library")
+  .then(() => console.log("Data base created"));
+
+
 
 //Server listning
-app.listen(port,()=>{
-    console.log(`Server listnig at port ${port}`)
-})
+app.listen(port, () => {
+  console.log(`Server listnig at port ${port}`);
+});
